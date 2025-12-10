3f8c198f8af5a3b41e9e93810f22f417d6ed0fe9
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-12-11 00:06:53 +0100

    feat: static styles

--- config/settings.py
+++ config/settings.py
@@ -138,11 +138,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
